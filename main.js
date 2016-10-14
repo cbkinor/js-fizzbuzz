38077802fe94b9212f5b1e512948bf7c64869b10
Chris Kuiper <chris.kuiper@gmail.com>
2016-10-14 09:42:24 -0500

    correctly create funtion using map/filter/reduce.

--- main.js
+++ main.js
@@ -15,9 +15,37 @@ const config = {
   ]
 }
 
-function fizzbuzz(config) {
-  // todo
+function fizzbuzz (config) {
+  const { from, until, factors } = config
+  const numbers = [...range(from, until)]
+  numbers
+    .filter(n => factors.some(arr => n % arr[0] === 0))
+    .map(factors.reduce(
+      (previous, next) =>
+        item =>
+          item % next[0] === 0
+            ? `${previous(item)}${next[1]}`
+            : previous(item),
+    item => `${item}: `
+  ))
+  .forEach(msg => console.log(msg))
 }
+
+
+
+// function fizzbuzzBroken (config) {
+
+//   let data = [...range(config.from, config.until)].map((item) => {}) {
+
+//     if (n % 5 === 0 && index % 3 === 0) return 'FizzBuzz'
+//     if (n % 5 === 0) return 'Buzz'
+//     if (n % 3 === 0) return 'Fizz'
+//     return n
+//   })
+//     .map(function (config) {
+//     console.log
+//   })
+// }
 
 // test
 fizzbuzz(config)
