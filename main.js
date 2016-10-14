0e89eed58703dc976a01cf49eae777c9e87eb826
Chris Kuiper <chris.kuiper@gmail.com>
2016-10-14 10:02:22 -0500

    add the funtional version for comparison.

--- main.js
+++ main.js
@@ -8,44 +8,53 @@ const range = function * (from, until) {
 
 const config = {
   from: 1,
-  until: 115,
+  until: 50,
   factors: [
     [3, 'fizz'],
     [5, 'buzz']
   ]
 }
-
-function fizzbuzz (config) {
+// function view
+function fizzbuzzFunctional (config) {
   const { from, until, factors } = config
   const numbers = [...range(from, until)]
   numbers
-    .filter(n => factors.some(arr => n % arr[0] === 0))
+    .filter(function (n) {
+      return factors
+      .some(arr => n % arr[0] === 0)
+    })
     .map(factors.reduce(
-      (previous, next) =>
-        item =>
-          item % next[0] === 0
-            ? `${previous(item)}${next[1]}`
-            : previous(item),
-    item => `${item}: `
+      function (prior, current) {
+        return function (n) {
+          if (n % current[0] === 0) {
+            return prior(n) + current[1]
+          } else {
+            return prior(n)
+          }
+        }
+      },
+      function (n) {
+        return n + `: `
+      }
   ))
   .forEach(msg => console.log(msg))
 }
 
-
-
-// function fizzbuzzBroken (config) {
-
-//   let data = [...range(config.from, config.until)].map((item) => {}) {
-
-//     if (n % 5 === 0 && index % 3 === 0) return 'FizzBuzz'
-//     if (n % 5 === 0) return 'Buzz'
-//     if (n % 3 === 0) return 'Fizz'
-//     return n
-//   })
-//     .map(function (config) {
-//     console.log
-//   })
-// }
+function fizzbuzz (config) {
+  const { from, until, factors } = config // sets constants for the config const
+  const numbers = [...range(from, until)] // sets constant for numbers from and until
+  numbers
+    .filter(n => factors.some(arr => n % arr[0] === 0))
+    .map(factors.reduce(
+      (previous, next) => // prior, current, returns a new function item
+        item =>
+          item % next[0] === 0 // check to see if current number is div by 3 or 5
+            ? `${previous(item)}${next[1]}` // ? stands for 'then return'
+            : previous(item), // : stands for 'else return'
+    item => `${item}: `
+  ))
+  .forEach(msg => console.log(msg))
+}
 
 // test
 fizzbuzz(config)
